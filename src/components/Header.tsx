2c1c69339a73081135ffd93b7fa10c1e7f79502b
Simonaglem <simonaglem@gmail.com>
2025-06-27 13:25:35 +0300

    Add homepage content; Fix sticky header, text color
    - Added main homepage content with hero and summary sections.
    - Fixed sticky header: styling was incorrectly applied to a child element
      instead of the header itself.
    - Fixed text color issue: a global * selector was overriding text color.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,11 +11,8 @@ export function Header() {
 	const isActive = (path: string) => location.pathname === path;
 
 	return (
-		<header className="bg-off-white flex items-center justify-center p-1 md:p-2 lg:p-4">
-			<nav
-				className="sticky top-0 flex items-start gap-8 p-4 md:gap-20 lg:gap-40"
-				aria-label="Main navigation"
-			>
+		<header className="bg-off-white text-dark-gray sticky top-0 flex items-center justify-center p-1 md:p-2 lg:p-4">
+			<nav className="flex items-start gap-8 p-4 md:gap-20 lg:gap-40" aria-label="Main navigation">
 				{links.map(({ label, path }) => (
 					<Link
 						key={path}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,34 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, Link } from '@tanstack/react-router';
+import profileImg from '../assets/profile.png';
 
 export const Route = createFileRoute('/')({
 	component: RouteComponent,
 });
 
 function RouteComponent() {
-	return <div>Hello "/"!</div>;
+	return (
+		<section>
+			<section className="page-intro-section hero-section">
+				<h1>Welcome to Simona's Portfolio</h1>
+				<em className="mb-6 p-2 text-sm md:text-base lg:text-lg">
+					Front-End Developer in the Making
+				</em>
+				<Link to="/projects" className="link-btn">
+					See My Work &#8674;
+				</Link>
+			</section>
+			<section className="page-main-section home-summary">
+				<img
+					src={profileImg}
+					alt="Profile Photo"
+					className="max-w-52 rounded-full md:max-w-60 lg:max-w-64"
+				/>
+				<p className="max-w-48 md:max-w-60 lg:max-w-80">
+					Aspiring front-end developer learning to build the web, one pixel at a time.
+					<br /> After working with ERP systems, I'm channeling my creativity into clean, functional
+					user interfaces.
+				</p>
+			</section>
+		</section>
+	);
 }
