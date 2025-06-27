c8b0aadb3b350a37abf470ca7abe660be2739c27
Simonaglem <simonaglem@gmail.com>
2025-06-27 17:18:00 +0300

    Change colors in 'Projects' page

--- src/routes/projects/route.tsx
+++ src/routes/projects/route.tsx
@@ -6,11 +6,11 @@ export const Route = createFileRoute('/projects')({
 
 function RouteComponent() {
 	return (
-		<section className="flex flex-col items-center justify-center p-8 text-center">
-			<h2 className="text-dark-gray p-4 text-base md:p-6 md:text-lg lg:p-10 lg:text-2xl">
+		<section className="bg-teal-green text-off-white flex flex-col items-center justify-center p-8 text-center">
+			<h2 className="p-4 text-base md:p-6 md:text-lg lg:p-10 lg:text-2xl">
 				404: Projects Not Found... Yet.
 			</h2>
-			<p className="text-dark-gray text-sm md:text-base lg:text-lg">
+			<p className="text-sm md:text-base lg:text-lg">
 				In the meantime, you can check out my GitHub profile for ongoing work:
 				<br />
 				<a
